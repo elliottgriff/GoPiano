372ac03e1faa51fcf3970d7ed91c6fc5aa1479b1
elliott <elliottcgriffin@gmail.com>
2018-12-04 16:40:53 -0500

    Initial Commit

---
+++ my_Piano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  my_Piano
+//
+//  Created by elliott on 12/4/18.
+//  Copyright © 2018 Elliott Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
